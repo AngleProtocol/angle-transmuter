a250ae74dd110b00e8eb43c9dde20365c9386963
Pablo Veyrat <veyratpablo@gmail.com>
2023-05-08 19:08:19 +0200

    fix: oracle values

--- contracts/kheops/libraries/LibOracle.sol
+++ contracts/kheops/libraries/LibOracle.sol
@@ -107,11 +107,7 @@ library LibOracle {
         oracleValue = read(readType, quoteType, data);
         uint256 _targetPrice = targetPrice(targetType);
         deviation = BASE_18;
-        if (oracleValue < _targetPrice) {
-            deviation = (oracleValue * BASE_18) / _targetPrice;
-            // Overestimating the oracle value
-            oracleValue = _targetPrice;
-        }
+        if (oracleValue < _targetPrice) deviation = (oracleValue * BASE_18) / _targetPrice;
     }
 
     // ============================== SPECIFIC HELPERS =============================
