101414866c17ca189d619b5414e2dc1ee0f537aa
picodes <picodes@angle.money>
2023-05-16 16:48:45 +0200

    fixes

--- contracts/kheops/facets/AccessControlModifiers.sol
+++ contracts/kheops/facets/AccessControlModifiers.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity ^0.8.12;
+pragma solidity ^0.8.17;
 
 import { LibDiamond } from "../libraries/LibDiamond.sol";
 
@@ -21,4 +21,3 @@ contract AccessControlModifiers {
         _;
     }
 }
-
--- contracts/kheops/libraries/LibHelpers.sol
+++ contracts/kheops/libraries/LibHelpers.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity ^0.8.12;
+pragma solidity ^0.8.17;
 
 import "oz/token/ERC20/utils/SafeERC20.sol";
 import "oz/utils/math/Math.sol";
@@ -102,4 +102,3 @@ library LibHelpers {
         }
     }
 }
-
