91e28f31a1613a1a20cac38fd1c1ea775037fb84
Pablo Veyrat <veyratpablo@gmail.com>
2023-06-06 17:52:34 +0200

    readding console

--- test/Fixture.sol
+++ test/Fixture.sol
@@ -20,6 +20,8 @@ import "contracts/utils/Errors.sol";
 
 import { Transmuter } from "./utils/Transmuter.sol";
 
+import { console } from "forge-std/console.sol";
+
 contract Fixture is Transmuter {
     IAccessControlManager public accessControlManager;
     ProxyAdmin public proxyAdmin;
