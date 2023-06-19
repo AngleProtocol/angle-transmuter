db5f379bc918a8f10caf751c19d4179134caa1ac
Pablo Veyrat <veyratpablo@gmail.com>
2023-06-19 12:40:50 +0200

    fix: comment

--- contracts/interfaces/IManager.sol
+++ contracts/interfaces/IManager.sol
@@ -5,7 +5,8 @@ pragma solidity >=0.5.0;
 /// @title IManager
 /// @author Angle Labs, Inc.
 interface IManager {
-    /// @notice Returns the value of collateral managed by the Manager, in agToken value
+    /// @notice Returns the amount of collateral managed by the Manager
+    /// @return balances Balances of all the subCollaterals handled by the manager
     /// @dev MUST NOT revert
     function totalAssets() external view returns (uint256[] memory balances, uint256 totalValue);
 
@@ -15,7 +16,7 @@ interface IManager {
     function invest(uint256 amount) external;
 
     /// @notice Sends `amount` of `collateral` to the `to` address
-    /// @dev Called when `agToken` are burned and during redemptions
+    /// @dev Called when `agToken` are burnt and during redemptions
     //  @dev MUST revert if there are not funds enough available
     /// @dev MUST be callable only by the transmuter
     function release(address asset, address to, uint256 amount) external;
