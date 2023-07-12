015e5c8318fd44ebaeaf30eaa6f6de4b3765d777
gs8nrv <55771972+GuillaumeNervoXS@users.noreply.github.com>
2023-07-12 17:27:45 +0200

    Diamond-etherscan (#73)

--- scripts/gnosis/GnosisDeployTransmuter.s.sol
+++ scripts/gnosis/GnosisDeployTransmuter.s.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.19;
 
-import { Utils } from "./Utils.s.sol";
+import { Utils } from "../Utils.s.sol";
 import { console } from "forge-std/console.sol";
+import { stdJson } from "forge-std/StdJson.sol";
 import "stringutils/strings.sol";
-import "./Constants.s.sol";
-import { CollateralSetupProd, Production } from "contracts/transmuter/configs/Production.sol";
+import { CollateralSetupProd, FakeGnosis } from "contracts/transmuter/configs/FakeGnosis.sol";
 import "contracts/transmuter/Storage.sol" as Storage;
 import { DiamondCut } from "contracts/transmuter/facets/DiamondCut.sol";
 import { DiamondLoupe } from "contracts/transmuter/facets/DiamondLoupe.sol";
@@ -17,38 +17,82 @@ import { SettersGovernor } from "contracts/transmuter/facets/SettersGovernor.sol
 import { SettersGuardian } from "contracts/transmuter/facets/SettersGuardian.sol";
 import { Swapper } from "contracts/transmuter/facets/Swapper.sol";
 import { ITransmuter } from "interfaces/ITransmuter.sol";
+import { MockTokenPermit } from "../../../test/mock/MockTokenPermit.sol";
+import { MockCoreBorrow } from "borrow/mock/MockCoreBorrow.sol";
+import { DummyDiamondImplementation } from "../generated/DummyDiamondImplementation.sol";
+import { MockChainlinkOracle } from "../../../test/mock/MockChainlinkOracle.sol";
 
-contract DeployTransmuter is Utils {
+contract GnosisDeployTransmuter is Utils {
     using strings for *;
+    using stdJson for string;
 
     address public config;
     string[] facetNames;
     address[] facetAddressList;
 
     function run() external {
-        uint256 deployerPrivateKey = vm.deriveKey(vm.envString("MNEMONIC_MAINNET"), 0);
+        uint256 deployerPrivateKey = vm.deriveKey(vm.envString("MNEMONIC_GNOSIS"), 0);
         vm.startBroadcast(deployerPrivateKey);
+
+        address deployer = vm.addr(deployerPrivateKey);
+        console.log("address: %s", deployer);
 
         /*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                                                         DEPLOY                                                      
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+        // Deploy fakes Core borrow, agEUR, and collaterals
+
+        MockCoreBorrow coreBorrow = new MockCoreBorrow();
+        console.log("CoreBorrow deployed at: %s", address(coreBorrow));
+        coreBorrow.toggleGovernor(deployer);
+
+        MockTokenPermit agEUR = new MockTokenPermit("Mock-AgEUR", "Mock-AgEUR", 18);
+        console.log("AgEUR deployed at: %s", address(agEUR));
+
+        address[] memory _collateralAddresses = new address[](3);
+        address[] memory _oracleAddresses = new address[](3);
+        for (uint256 i; i < 3; i++) {
+            MockTokenPermit collateral = new MockTokenPermit(
+                string(abi.encodePacked("MockCollat", vm.toString(i))),
+                string(abi.encodePacked("MockCollat", vm.toString(i))),
+                uint8(6 * (i + 1))
+            );
+            _collateralAddresses[i] = address(collateral);
+            console.log("Collateral %i deployed at: %s", i, address(collateral));
+
+            MockChainlinkOracle oracle = new MockChainlinkOracle();
+            console.log("oracle deployed at: %s", address(oracle));
+            oracle.setLatestAnswer(1e8);
+            _oracleAddresses[i] = address(oracle);
+        }
 
         // Config
-        config = address(new Production());
+        config = address(new FakeGnosis());
         ITransmuter transmuter = _deployTransmuter(
             config,
-            abi.encodeWithSelector(Production.initialize.selector, CORE_BORROW, AGEUR)
+            abi.encodeWithSelector(
+                FakeGnosis.initialize.selector,
+                coreBorrow,
+                agEUR,
+                _collateralAddresses,
+                _oracleAddresses
+            )
         );
 
         console.log("Transmuter deployed at: %s", address(transmuter));
-        vm.stopBroadcast();
 
         /*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                                                     TODO GOVERNANCE                                                 
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
         // Redeem
-        // SettersGuardian.setRedemptionCurveParams(xRedeemFee, yRedeemFee);
-        // SettersGuardian.togglePause(collaterals[0].token, ActionType.Redeem);
+        uint64[] memory xRedeemFee = new uint64[](1);
+        int64[] memory yRedeemFee = new int64[](1);
+        xRedeemFee[0] = 0;
+        yRedeemFee[0] = 1e9;
+        transmuter.setRedemptionCurveParams(xRedeemFee, yRedeemFee);
+        transmuter.togglePause(address(0x0), Storage.ActionType.Redeem);
+
+        vm.stopBroadcast();
     }
 
     /*//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -70,9 +114,6 @@ contract DeployTransmuter is Utils {
         facetNames.push("Getters");
         facetAddressList.push(address(new Getters()));
 
-        facetNames.push("Redeemer");
-        facetAddressList.push(address(new Redeemer()));
-
         facetNames.push("RewardHandler");
         facetAddressList.push(address(new RewardHandler()));
 
@@ -85,14 +126,28 @@ contract DeployTransmuter is Utils {
         facetNames.push("Swapper");
         facetAddressList.push(address(new Swapper()));
 
+        // TODO when deploying don't forget to regenerate this contract if you
+        // changed any code on the Transmuter
+        facetNames.push("DiamondEtherscanFacet");
+        DummyDiamondImplementation dummyImpl = new DummyDiamondImplementation();
+
+        // Putting it at the end as it is the one failing when verifying on etherscan
+        facetNames.push("Redeemer");
+        facetAddressList.push(address(new Redeemer()));
+
+        string memory json = vm.readFile(JSON_SELECTOR_PATH);
         // Build appropriate payload
         uint256 n = facetNames.length;
         Storage.FacetCut[] memory cut = new Storage.FacetCut[](n);
         for (uint256 i = 0; i < n; ++i) {
+            // Get Selectors from json
+            bytes4[] memory selectors = _arrayBytes32ToBytes4(
+                json.readBytes32Array(string.concat("$.", facetNames[i]))
+            );
             cut[i] = Storage.FacetCut({
                 facetAddress: facetAddressList[i],
                 action: Storage.FacetCutAction.Add,
-                functionSelectors: _generateSelectors(facetNames[i])
+                functionSelectors: selectors
             });
         }
 
