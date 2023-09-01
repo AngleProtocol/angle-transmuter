3628598a9b23ef84960fa29b2266567c7070b04f
Pablo Veyrat <veyratpablo@gmail.com>
2023-09-01 10:39:30 +0200

    feat: mainnet deployment and prepare deployment on other chains

---
+++ scripts/ConstantsAvalanche.s.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.19;
+
+import "contracts/utils/Constants.sol";
+
+address constant IMMUTABLE_CREATE2_FACTORY_ADDRESS = 0x0000000000FFe8B47B3e2130213B802212439497;
---
+++ scripts/ConstantsOptimism.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.19;
+
+import "contracts/utils/Constants.sol";
+
+address constant IMMUTABLE_CREATE2_FACTORY_ADDRESS = 0x0000000000FFe8B47B3e2130213B802212439497;
+
+address constant CHAIN_AGEUR = 0x9485aca5bbBE1667AD97c7fE7C4531a624C8b1ED;
+address constant PROXY_ADMIN = 0xC16B81Af351BA9e64C1a069E3Ab18c244A1E3049;
+address constant ACCESS_CONTROL_MANAGER = 0x1a7e4e63778B4f12a199C062f3eFdD288afCBce8;
