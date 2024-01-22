fcd4c62d171b04b6334766ea6547bacbb5b450d6
Pablo Veyrat <veyratpablo@gmail.com>
2024-01-22 14:53:00 +0100

    feat: transmuter

--- contracts/transmuter/configs/ProductionUSD.sol
+++ contracts/transmuter/configs/ProductionUSD.sol
@@ -2,26 +2,7 @@
 
 pragma solidity ^0.8.19;
 
-import "interfaces/external/chainlink/AggregatorV3Interface.sol";
-
-import { LibDiamondEtherscan } from "../libraries/LibDiamondEtherscan.sol";
-import "../libraries/LibOracle.sol";
-import { LibSetters } from "../libraries/LibSetters.sol";
-import { LibStorage as s } from "../libraries/LibStorage.sol";
-
-import { DummyDiamondImplementation } from "../../../scripts/generated/DummyDiamondImplementation.sol";
-
-import "../../utils/Constants.sol";
-import "../Storage.sol" as Storage;
-
-struct CollateralSetupProd {
-    address token;
-    bytes oracleConfig;
-    uint64[] xMintFee;
-    int64[] yMintFee;
-    uint64[] xBurnFee;
-    int64[] yBurnFee;
-}
+import "./ProductionTypes.sol";
 
 /// @dev This contract is used only once to initialize the diamond proxy.
 contract ProductionUSD {
