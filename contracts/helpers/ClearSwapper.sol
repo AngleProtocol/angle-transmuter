7fa3f171e4c81bc4933adb4a5835ac6f2c155964
Pablo Veyrat <veyratpablo@gmail.com>
2024-10-24 13:25:44 +0200

    feat: add helpers

--- contracts/helpers/ClearSwapper.sol
+++ contracts/helpers/ClearSwapper.sol
@@ -72,6 +72,54 @@ contract ClearSwapper is AccessControl {
         if (amountOut < minAmountOut) revert TooSmallAmountOut();
     }
 
+    function deposit(
+        address tokenIn,
+        uint256 amountIn,
+        uint256 minAmountOut,
+        address to,
+        uint256 deadline,
+        address stablecoin
+    ) external returns (uint256) {
+        StableContracts memory stableData = stableContracts[stablecoin];
+        return swap(tokenIn, stableData.savings, amountIn, minAmountOut, to, deadline, stablecoin);
+    }
+
+    function redeem(
+        address tokenOut,
+        uint256 amountIn,
+        uint256 minAmountOut,
+        address to,
+        uint256 deadline,
+        address stablecoin
+    ) external returns (uint256) {
+        StableContracts memory stableData = stableContracts[stablecoin];
+        return swap(stableData.savings, tokenOut, amountIn, minAmountOut, to, deadline, stablecoin);
+    }
+
+    function withdraw(
+        address tokenOut,
+        uint256 amountOut,
+        uint256 maxAmountIn,
+        address to,
+        uint256 deadline,
+        address stablecoin
+    ) external returns (uint256) {
+        StableContracts memory stableData = stableContracts[stablecoin];
+        return swapExactOutput(stableData.savings, tokenOut, amountOut, maxAmountIn, to, deadline, stablecoin);
+    }
+
+    function mint(
+        address tokenIn,
+        uint256 amountOut,
+        uint256 maxAmountIn,
+        address to,
+        uint256 deadline,
+        address stablecoin
+    ) external returns (uint256) {
+        StableContracts memory stableData = stableContracts[stablecoin];
+        return swapExactOutput(tokenIn, stableData.savings, amountOut, maxAmountIn, to, deadline, stablecoin);
+    }
+
     function swapExactOutput(
         address tokenIn,
         address tokenOut,
@@ -80,7 +128,7 @@ contract ClearSwapper is AccessControl {
         address to,
         uint256 deadline,
         address stablecoin
-    ) external returns (uint256 amountIn) {
+    ) public returns (uint256 amountIn) {
         amountIn = quoteOut(tokenIn, tokenOut, amountOut, stablecoin);
         if (amountIn > maxAmountIn) revert TooBigAmountIn();
         swap(tokenIn, tokenOut, amountIn, amountOut, to, deadline, stablecoin);
@@ -116,6 +164,26 @@ contract ClearSwapper is AccessControl {
             amountIn = ITransmuter(stableData.transmuter).quoteOut(amountOut, stablecoin, tokenOut);
             amountIn = IERC4626(stableData.savings).previewWithdraw(amountIn);
         }
+    }
+
+    function previewDeposit(address tokenIn, uint256 amountIn, address stablecoin) external view returns (uint256) {
+        StableContracts memory stableData = stableContracts[stablecoin];
+        return quoteIn(tokenIn, stableData.savings, amountIn, stablecoin);
+    }
+
+    function previewWithdraw(address tokenOut, uint256 amountIn, address stablecoin) external view returns (uint256) {
+        StableContracts memory stableData = stableContracts[stablecoin];
+        return quoteIn(stableData.savings, tokenOut, amountIn, stablecoin);
+    }
+
+    function previewMint(address tokenIn, uint256 amountOut, address stablecoin) external view returns (uint256) {
+        StableContracts memory stableData = stableContracts[stablecoin];
+        return quoteOut(tokenIn, stableData.savings, amountOut, stablecoin);
+    }
+
+    function previewRedeem(address tokenOut, uint256 amountOut, address stablecoin) external view returns (uint256) {
+        StableContracts memory stableData = stableContracts[stablecoin];
+        return quoteOut(stableData.savings, tokenOut, amountOut, stablecoin);
     }
 
     function setStableContracts(address stablecoin, address transmuter, address savings) external onlyGuardian {
